6fb91f37e8aadaff1d5c5cb756086c4b3c65d49a
Fatima zahra majdou <146893864+Fatimazahra-21@users.noreply.github.com>
2023-10-09 02:17:25 +0300

    Create tax.py

---
+++ tax.py
@@ -0,0 +1,6 @@
+sexe = input("entrer le sexe de habitant :")
+age = int(input("entrer age de habitant :")
+if ((sexe="H" and age >=20) or (sexe="F" and age>= 18 and age <= 35)) :
+print("habitant est imposable")
+else:
+print("habitant est non imposable")
